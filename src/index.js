9d095c786df5d4dd126ed6b4b3f41edcc08efdc5
Yoga2015 <yoga3333@126.com>
2023-03-12 12:52:20 +0800

    新增 Source Map：可以定位报错的具体行数，且不想暴露源码

--- src/index.js
+++ src/index.js
@@ -10,5 +10,19 @@ import './css/index.less';
 $(function () {
   // 实现奇偶行变色     // odd 和 even的索引是从0开始的 。  0 是 偶数 ，  1 是 奇数
   $('li:odd').css('backgroundColor', 'yellow');    // 奇数行 为 粉红色  
-  $('li:even').css('backgroundColor', 'green');  // 偶数行 为 黄色
+  $('li:even').css('backgroundColor', 'red');  // 偶数行 为 黄色
 })
+
+
+
+// 定义一个普通的类
+class Person {
+  // 通过 static 关键字，为 Person 类定义了一个静态属性  info
+  // webpack 无法打包处理 '静态属性'这个高级语法
+  static info = 'person info'
+};
+
+// console.log(Person.info);
+// 故意写错，重新 yarn run dev, 看看有没有 定位到 具体的源代码
+consle.log(Person.info);
+
--- webpack.config.js
+++ webpack.config.js
@@ -10,10 +10,20 @@ const HtmlPlugin = require('html-webpack-plugin');
 const htmlPlugin = new HtmlPlugin({
   template: './src/index.html',  // 指定 要复制 哪个页面
   filename: './index.html',  // 指定 复制出来的 文件名 和 存放路径
-})
+});
+
+// 13、clean-webpack-plugin 按需 导入插件、得到插件的构造函数之后，创建插件的实例对象
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const cleanPlugin = new CleanWebpackPlugin();
 
 // 1、使用 Commonjs规范 的导出语法，向外共享 一个 webpack 的配置对象
 module.exports = {
+
+  // 14、eval-source-map 仅限 在 开发环境 下使用，不建议在 生产环境 下使用
+  // 此选项 生成的 Source Map 能够保证 运行时报错的行数 与 源代码的行数  保持一致
+  // devtool: 'eval-source-map',
+  // 将 devtool 的值设置为 nosources-source-map 可以定位报错的具体行数，且不想暴露源码
+  devtool: 'nosources-source-map',
 
   // 2、mode 用来指定构建模式。 可选值 有 development 和 production
   mode: 'development',
@@ -24,7 +34,7 @@ module.exports = {
   // 5、output 出口文件 : 指定 生成的文件 存放到哪里
   output: {
     path: path.join(__dirname, './dist'), // 存放到哪个目录
-    filename: 'bundle.js',  //生成的 文件名
+    filename: 'js/bundle.js',  //生成的 文件名
   },
 
   // 8、插件的数组 ，将来 webpack 在运行时，会 加载 并 调用 这些插件
@@ -32,6 +42,7 @@ module.exports = {
   plugins: [
     htmlPlugin,
     // new UglifyJsPlugin(),   // 11、 添加 压缩功能的plugin
+    cleanPlugin
   ],
 
   // 9、devServer 节点进行配置，能帮我们 在项目初次打包完成后,自动打开浏览器，并自动访问 指定页面
@@ -45,21 +56,51 @@ module.exports = {
   module: {
     // 子节点 rules 中 指定 文件后缀名的 匹配规则， 定义了不同后缀名的的模块 对应的loader
     rules: [
+
       // (1)、处理 .css 文件的loader
       {
         test: /\.css$/,    // test 用来 匹配的文件类型
         use: ['style-loader', 'css-loader'],   // use 用来指定 对应 要调用的loader 
       },
+
       // (2)、处理 .less 文件的loader
       {
         test: /\.less$/,
         use: ['style-loader', 'css-loader', 'less-loader'],
       },
+
       // (3)、处理 图片文件的loader
+      // 如果 需要调用的loader 只有一个，则只传递一个字符串也行，如果多个loader , 则必须指定 数组 装着
+      // {
+      //   test: /\.jpg|png|gif$/,   // 找到 符合格式的图片后，用 use 来处理
+      //   use: 'url-loader?limit=22229',  // 其中 ? 之后的是 loader 的参数项 
+      // }
       {
         test: /\.jpg|png|gif$/,   // 找到 符合格式的图片后，用 use 来处理
-        use: 'url-loader?limit=22229',  // 其中 ? 之后的是 loader 的参数项 
-      }
+        use: {
+          loader: 'url-loader',    // 通过 loader 属性 指定要调用的 loader
+          options: {         // 通过 options属性指定参数项
+            limit: 22229,
+            outputPath: 'image'
+          }
+        }
+      },
+
+      // (4)、处理js高级语法
+      {
+        test: /\.js$/,
+        // exclude 为排除项   不包括
+        // 表示 babel-loader 只需处理开发者编写的 js 文件，不需要处理 node_modules 下的js文件
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',     //由 babel-loader 来调用 @babel/plugin-proposal-class-properties
+          options: {                // 参数项
+            // 声明一个 babel 插件 ，此插件用来转化 class 中的高级语法
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        },
+      },
+
     ]
   },
 }
